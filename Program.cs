65144752200b37a765c084a276fb7f6c3df8cbb1
clmiranda <miranda76575@gmail.com>
2024-04-01 17:43:17 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using ClienteProtectora;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44398/") });
+
+await builder.Build().RunAsync();
